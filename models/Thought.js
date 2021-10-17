8f33a4bb4468690672dba21236990f03546084da
bill geng <billgeng@yahoo.com>
2021-10-16 23:21:15 -0400

    some errors

--- models/Thought.js
+++ models/Thought.js
@@ -2,24 +2,25 @@ const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
 
 
-const ReactionSchema = new Schema({
-    reactonId:{
-        type: Types.ObjectId,
-        default: new Types.ObjectId(),
+const ReactionSchema = new Schema(
+    {
+        reactionId:{
+            type: Types.ObjectId,
+            default: new Types.ObjectId(),
     },
-    reactionBody: {
-        type:String,
-        required: true,
-        maxLength: 280
+        reactionBody: {
+         type:String,
+         required: true,
+         maxLength: 280
     },
-    username: {
-        type: String,
-        required: true
+        username: {
+            type: String,
+            required: true
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-        get: (createdAtVal) => moment(createdAtVal).format('YYYY-MM-DD [at] hh:mm a')
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('YYYY-MM-DD [at] hh:mm a')
     },
 
 }, 
--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ const UserSchema = new Schema({
 {
     toJSON: {
         virtuals:true,
-        getters: true
+        
     },
     id: false,
 }
